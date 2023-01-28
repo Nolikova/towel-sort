b7f41bf4fbb3f090c2280ffb760f89addb2b859d
Anzhelika Novikova <anzhelika.novikova93@gmail.com>
2023-01-28 16:22:16 +0400

    feat: add towel sort solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix?.length) return [];
+
+  const res = matrix.reduce((acc, m, i) => {
+    acc.push(i % 2 === 0 ? m : m.reverse());
+    return acc;
+  }, []);
+
+  return res.flat();
 }
